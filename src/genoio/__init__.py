c4b1a600685b67d422ae2a2f70636f6d1191d048
quattro <nick.mancuso@gmail.com>
2026-07-02 16:33:10 -0700

    chore: release 0.3.0

--- src/genoio/__init__.py
+++ src/genoio/__init__.py
@@ -34,7 +34,7 @@ from ._filters import (
 try:
     __version__ = version("genoio")
 except PackageNotFoundError:
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
 
 __all__ = [
     "__version__",
